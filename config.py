a909d418fd8b87cd80dd6fe3193b1338d20b9553
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2019-08-09 21:45:29 +0530

    monolithic config file

--- config.py
+++ config.py
@@ -1,20 +1,37 @@
-import torch.nn as nn
-import torch
+import numpy as np
 
 
-conf = {
-    'batch_size' : 256,
-    'loss_fn' : nn.MSELoss(),
-    'optim' : torch.optim.Adam,
-    'epochs' : 100,
-    'model'  : 'ffn',
-    'data'   : 'bec2d',
-    'num_samples' : 1024,
-    'net'    : 'ffn',
-    'nn' : {
-      'dim_in' : 1, 'dim_hid' : 750, 'dim_out' : 512
-      },
-    'g_low'  : 90,
-    'g_high' : 100,
-    'save_every' : 16
-    }
+"""
+class Config:
+    batch_size = 256
+    loss_fn = nn.MSELoss()
+    optim = torch.optim.Adam
+    epochs = 100
+    model = 'ffn'
+    data = 'bec2d'
+    num_samples = 1024
+    net = 'ffn'
+    nn = {
+      'dim_in' : 1, 'dim_hid' : 750, 'dim_out' : 51
+      }
+    g_low = 90
+    g_high = 100
+    save_every = 16
+"""
+
+
+class ConfigHarmonic:
+  dim = 512
+  radius = 24
+  angular_momentum = 1
+  time_step = 1e-4
+  coupling = 1
+  potential_fn = None
+  iterations = 10000
+  coupling_vars = [1, 300]  # , 100]
+
+  def wave_function(r):  # constant
+    return 1. / np.sqrt(24)
+
+  def potential_fn(x, y):  # harmonic potential
+    return 0.5 * (x ** 2 + y ** 2)
