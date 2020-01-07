f00825c7810408238d7b3c69c91e0bf795d139a9
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2020-01-07 06:17:20 +0530

    update x range and steps

--- gpbec/params.py
+++ gpbec/params.py
@@ -8,6 +8,8 @@ c1 = {
     'iterations' : 100000,
     'potential' : pot.harmonic,
     'wave_function' : pot.gaussian,
+    'x.low' : -11.9765625, 'x.high' : 12, 'x.step' : 0.04673793859649123,
+    'g.low' : 0, 'g.high' : 100, 'g.step' : 0.1,
     }
 
 c2 = {
@@ -16,7 +18,12 @@ c2 = {
     'iterations' : 100000,
     'g11' : 1., 'g12' : 1., 'g22' : 1.,  # coupling strengths
     'potential' : pot.harmonic,
-    'omega' : -1
+    'omega' : -1, 'omega.low' : -20,
+    'omega.high' : -1,
+    'x.low' : -12, 'x.high' : 12, 'x.step' : 0.1,
+    'g11.low' : 0, 'g11.high' : 120, 'g11.step' : 0.1,
+    'g12.low' : 0, 'g12.high' : 120, 'g12.step' : 0.1,
+    'g22.low' : 0, 'g22.high' : 120, 'g22.step' : 0.1,
     }
 
 
